a7d5a01817db13c420e326c10e815670212c30f9
Zingbefinance <zingbefinance123@gmail.com>
2026-06-26 19:00:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+
 from config import CHECK_INTERVAL, POST_INTERVAL
 from price import get_price
-from telegram_sender import send_message
+from telegram_sender import send_or_edit_message
 import time
 
 print("🚀 ZinBot démarré")
@@ -15,10 +16,19 @@ while True:
             current_time = time.time()
 
             if current_time - last_post >= POST_INTERVAL:
-                message = f"💰 ZING TOKEN\n\nPrix actuel : ${data['price']}"
-                send_message(message)
-                print("Message Telegram envoyé.")
+
+                message = f"""💰 <b>ZING TOKEN</b>
+
+📈 Prix actuel : <b>${data['price']}</b>
+⏰ Mise à jour automatique
+
+🔗 https://dexscreener.com/solana/{data['pair']}
+"""
+
+                send_or_edit_message(message)
                 last_post = current_time
+                print("✅ Message Telegram mis à jour.")
+
         else:
             print("Le token n'a pas encore de paire active.")
 
