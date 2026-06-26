d4697422d0ea09c35b4391295bc35eb302035724
Zingbefinance <zingbefinance123@gmail.com>
2026-06-26 19:08:17 +0000

    Refactor MINT_ADDRESS to use os.getenv
    Updated MINT_ADDRESS to use environment variable.

--- price.py
+++ price.py
@@ -1,9 +1,9 @@
 import requests
+import os
 
-MINT_ADDRESS = "4zihBzwHLx9z7aNmXam181iUd285xbqJNN57M5LhoHpu"
+MINT_ADDRESS = os.getenv("MINT_ADDRESS")
 
 def get_price():
-
     url = f"https://api.dexscreener.com/latest/dex/tokens/{MINT_ADDRESS}"
 
     r = requests.get(url)
@@ -25,8 +25,3 @@ def get_price():
         "volume24h": pair["volume"]["h24"],
         "pair": pair["url"]
     }
-
-
-if __name__ == "__main__":
-
-    print(get_price())
