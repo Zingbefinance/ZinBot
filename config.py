759a8b7260548b6ab9007596118038108c74950d
Zingbefinance <zingbefinance123@gmail.com>
2026-06-26 15:37:04 +0000

    Add configuration parameters for price checks and Telegram

---
+++ config.py
@@ -0,0 +1,11 @@
+# Temps entre deux vérifications du prix (en secondes)
+CHECK_INTERVAL = 60
+
+# Temps entre deux publications Telegram (30 minutes)
+POST_INTERVAL = 1800
+
+# Nom du token
+TOKEN_NAME = "ZING TOKEN"
+
+# Symbole
+TOKEN_SYMBOL = "ZING"
