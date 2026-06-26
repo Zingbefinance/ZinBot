d765875aa9f139f9db0eeeae2dde2f6256522578
Zingbefinance <zingbefinance123@gmail.com>
2026-06-26 13:49:18 +0000

    Add Telegram message sending functionality

---
+++ telegram.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_message(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+
+    response = requests.post(url, data=payload)
+    return response.json()
